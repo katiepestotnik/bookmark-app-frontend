6e7e992034303f440018d08ecd245a17a573f856
Katie Pestotnik <katie.pestotnik@gmail.com>
2021-11-20 21:39:54 -0700

    trying to revert

--- src/App.js
+++ src/App.js
@@ -1,51 +1,12 @@
 import './App.css';
 import Header from './components/Header';
 import Main from './components/Main';
-import {createContext, useState, useEffect} from 'react'
-import { Route, Link, Switch } from 'react-router-dom';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
-import Home from './pages/Home'
-export const GlobalCtx = createContext(null);
-
 function App() {
-  const [globalState, setGlobalState] = useState({
-    //herokuapp link here
-    url: "http://localhost:3000", token: null
-  });
-  //Seeing if logged in
-  useEffect(() => {
-    const token = JSON.parse(window.localStorage.getItem("token"))
-    console.log(token)
-    if (token) {
-      setGlobalState({ ...globalState, token: token.token })
-    }
-  },[])
   return (
-    <GlobalCtx.Provider value={{globalState, setGlobalState}}>
-      <div className="App full-page-style">
-        <Link to="/">
-          <h1>Bookmark'd</h1>
-        </Link>
-        <Header />
-        <main>
-          <Switch>
-            <Route
-              exact path="/"
-              render={(rp) => globalState.token?<Main/>:<Home/>}>
-            </Route>
-            <Route
-              path="/signup"
-              render={(rp) => <Signup {...rp}/>}>
-            </Route>
-            <Route
-              path="/login"
-              render={(rp) => <Login {...rp}/>}>
-            </Route>
-          </Switch>
-        </main>
+    <div className="App full-page-style">
+      <Header />
+      <Main />
     </div>
-    </GlobalCtx.Provider>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ const Home = (props) => {
     return (
         <div>
             <h1>Welcome</h1>
-            <p>Signup/Login to view Main</p>
+            <h3>Signup/Login to go to dashboard</h3>
         </div>
     )
 }
